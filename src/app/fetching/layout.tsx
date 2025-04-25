dd747a77155564eea797bb4b4add5e08c7412ee2
Miguel Caballero <mcabs3@gmail.com>
2025-04-24 21:11:56 -0500

    cleaned up styling

---
+++ src/app/fetching/layout.tsx
@@ -0,0 +1,12 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="px-8 pt-20 pb-10">
+      <header>
+        <span className="font-mono text-neutral-600 tracking-tight">
+          ~/fetching
+        </span>
+      </header>
+      {children}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="pt-20 pb-10 px-8">
       <h1>Home</h1>
       <blockquote>
         This page fetches no content and can be built as static
