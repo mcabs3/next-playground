3382d2de6f8efc6a20517cac50ea16c1feab0127
Miguel Caballero <mcabs3@gmail.com>
2025-04-30 13:34:20 -0500

    enabled ppr

--- src/app/fetching/layout.tsx
+++ src/app/fetching/layout.tsx
@@ -8,6 +8,27 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <h1>Fetching Patterns</h1>
       </header>
       {children}
+      <footer className="pt-10">
+        <span className="text-2xl pb-4 inline-block">Resources</span>
+        <ul>
+          <li>
+            <a
+              className="underline hover:no-underline"
+              href="https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming"
+            >
+              Next.js - Streaming
+            </a>
+          </li>
+          <li>
+            <a
+              className="underline hover:no-underline"
+              href="https://react.dev/reference/react/Suspense"
+            >
+              React - Suspense
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
--- src/app/fetching/page.tsx
+++ src/app/fetching/page.tsx
@@ -1,13 +1,7 @@
 export default function Page() {
   return (
     <main>
-      <blockquote data-level="green">
-        This page fetches no content and can be built as static
-      </blockquote>
-      <p>
-        Select any of the options above to see different patterns on how you can
-        fetch data for a page.
-      </p>
+      <p>Select a data fetching pattern in the left to explore.</p>
     </main>
   );
 }
