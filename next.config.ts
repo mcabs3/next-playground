3c8b4683fde22bdff9f0cbd2146dd27031ed7e5a
Miguel Caballero <mcabs3@gmail.com>
2025-04-29 22:18:47 -0500

    added all experimental features back

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    // ppr: true,
-    //   dynamicIO: true,
-    //   useCache: true,
+    ppr: true,
+    dynamicIO: true,
+    useCache: true,
     clientSegmentCache: true,
   },
 };
