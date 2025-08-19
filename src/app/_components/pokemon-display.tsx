27a27cb51b94ee07e1163b735878d768f45570f9
Miguel Caballero <mcabs3@gmail.com>
2025-08-19 15:07:23 -0500

    updated pokemon display

--- src/app/_components/pokemon-display.tsx
+++ src/app/_components/pokemon-display.tsx
@@ -15,14 +15,16 @@ export function PokemonDisplay({
 
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col-reverse items-center border px-2 py-2 rounded-lg">
-        <p className="capitalize">{pokemon?.name}</p>
-        <Image
-          width={100}
-          height={100}
-          src={pokemon?.sprites?.front_default}
-          alt={pokemon?.name}
-        />
+      <div className="flex flex-col-reverse items-center border-4 border-[#FEC201] px-4 py-4 rounded-lg gap-8 bg-blue-900 shadow">
+        <p className="capitalize font-bold">{pokemon?.name}</p>
+        <div className="border-2 border-[#FEC201] rounded py-2 px-4 bg-neutral-900 shadow-sm">
+          <Image
+            width={100}
+            height={100}
+            src={pokemon?.sprites?.front_default}
+            alt={pokemon?.name}
+          />
+        </div>
       </div>
     </div>
   );
