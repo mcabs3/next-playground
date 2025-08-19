472493cfb3a2232fe2720bd4c81e089b74b6fabc
Miguel Caballero <mcabs3@gmail.com>
2025-08-19 15:12:05 -0500

    disable experimental features

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    ppr: true,
-    clientSegmentCache: true,
-    viewTransition: true,
+    // ppr: true,
+    // clientSegmentCache: true,
+    // viewTransition: true,
   },
   images: {
     remotePatterns: [
